dfb653683f0e596a446565b6a50518b5e1d29558
alu53381650f <alu53381650f@ti3-04>
2015-05-18 15:33:56 +0200

    18 MAY 15

---
+++ HotelSoftware/src/com/amo11k/hotel/HotelTest.java
@@ -0,0 +1,24 @@
+package com.amo11k.hotel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HotelTest {
+
+	@Test
+	public void testHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRoomAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDisponibleInt() {
+		fail("Not yet implemented");
+	}
+
+}
