b63b63c211c9a4cbbbe5d5fc80d153c9a15bed47
alu53381650f <alu53381650f@ti3-04>
2015-05-18 15:42:26 +0200

    18 may 15

---
+++ HotelSoftware/src/com/amo11k/hotel/ReservaTest.java
@@ -0,0 +1,29 @@
+package com.amo11k.hotel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReservaTest {
+
+	@Test
+	public void testReserva() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ HotelSoftware/src/com/amo11k/hotel/RoomTest.java
@@ -0,0 +1,54 @@
+package com.amo11k.hotel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoomTest {
+
+	@Test
+	public void testRoom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRoomIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTypeInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSmoke() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSmokeBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDisponible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDisponible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
